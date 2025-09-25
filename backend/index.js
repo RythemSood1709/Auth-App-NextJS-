fff7728651649630298a0535928a0a6961bc4582
Rythem Sood <rythemsoodlm10@gmail.com>
2025-09-26 01:40:50 +0530

    Authentication Backend Completed

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express=require('express');
 
 const app=express();
+const bodyParser= require('body-parser')
+const cors=require('cors');
+const authRoutes=require('./src/routes/authRoutes')
 
 require('dotenv').config();
 require('./src/models/db');
@@ -10,6 +13,12 @@ const PORT=process.env.PORT|| 8080;
 app.get('/',(req,res)=>{
     res.send('Hello World!');
 });
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.use('/auth',authRoutes);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
